ce6823dc09c0b5021c53a152701504dd4f0e5e35
TheSniezek <maciek.wisniewski16@gmail.com>
2024-10-12 11:10:38 +0200

    c

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,10 @@ export const Header = () => {
     <div className="header-container">
       <div className="header-content-container">
         <div className="header-title">TheSniezek's Shop</div>
+        <div className="disclaimer">
+          <div className="disclaimer-title">DISCLAIMER</div> <br /> I don't have private server{' '}
+          <br /> I'm not responsive if someone bids higher than you.
+        </div>
       </div>
     </div>
   );
