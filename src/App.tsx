5efcbad2e7a6f566178b1dd8171cf75634f5e5e0
TheSniezek <maciek.wisniewski16@gmail.com>
2025-04-05 20:36:47 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import SectionManager from './components/SectionManager/SectionManager';
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <SectionManager />
     </div>
   );
